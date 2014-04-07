ac3dae13ff8d8b1baf6f8df661d7d9bad8d04fcb
Ian Fox <ianfox41@gmail.com>
2014-04-07 03:31:20 -0700

    objective c and scode proj added

--- README.md
+++ README.md
@@ -5,14 +5,4 @@ This is the native iOS app to compliment the MapBox markers example:
  * Repo: https://github.com/mapbox/weekend-picks-template
  * Blog post: http://mapbox.com/blog/markers
 
-This is a native iOS app with similar functionality. Once you've got a map with markers on it, just configure the following in `MBWPViewController.m`: 
-
- * `kNormalMapID`: Map ID for your map on MapBox
- * `kRetinaMapID`: Map ID for your retina-compatible map on MapBox (see [Retina support](http://mapbox.com/mapbox-ios-sdk/#retina_support))
- * `kTintColorHex`: HTML/CSS-style color hex code to match theming with your site
-
-Then, just build & run the app and explore your picks! 
-
-![](screenshot.png)
-
-Quick demo video: https://dl.dropbox.com/u/575564/picksvid.mov
+Modifications allows a user in the bay area to report late Bart. A user can also search for a specific bart locatio.
--- Weekend Picks/MBWPViewController.m
+++ Weekend Picks/MBWPViewController.m
@@ -2,7 +2,7 @@
 //  MBWPViewController.m
 //  Weekend Picks
 //
-//  Created by Justin Miller on 6/15/12.
+//  Modified by Ian Fox 5/6/2014
 //  Copyright (c) 2012 MapBox / Development Seed. All rights reserved.
 //
 
@@ -13,92 +13,90 @@
 
 #import "UIColor+MBWPExtensions.h"
 
-#define kNormalMapID  @"examples.map-zr0njcqy"
-#define kRetinaMapID  @"examples.map-zjm2w6i9"
+#define kNormalMapID  @"foxxintheboxx.hneeig66"
+#define kRetinaMapID  @"foxxintheboxx.hneeig66"
 #define kTintColorHex @"#AA0000"
 
-@interface MBWPViewController ()
-
+@interface MBWPViewController () <UISearchBarDelegate>
+@property  (strong) NSMutableDictionary *annotations;
 @property (strong) IBOutlet RMMapView *mapView;
+@property  (strong) NSMutableDictionary *markers;
 @property (strong) NSArray *activeFilterTypes;
+
+@property (strong, nonatomic) IBOutlet UISearchBar *search;
 
 @end
 
 #pragma mark -
 
 @implementation MBWPViewController
-
+@synthesize markers;
+@synthesize annotations;
 @synthesize mapView;
 @synthesize activeFilterTypes;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-
-    self.navigationController.navigationBar.tintColor = [UIColor colorWithHexString:kTintColorHex];
+    self.search.showsCancelButton = YES;
+    [self.search setShowsScopeBar: YES];
+    self.search.delegate = self;
     
-    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemSearch 
-                                                                                           target:self 
-                                                                                           action:@selector(presentSearch:)];
-    
-    self.navigationItem.leftBarButtonItem = [[RMUserTrackingBarButtonItem alloc] initWithMapView:self.mapView];
-    self.navigationItem.leftBarButtonItem.tintColor = self.navigationController.navigationBar.tintColor;
-
-    self.navigationItem.backBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Map" style:UIBarButtonItemStyleBordered target:nil action:nil];
-
-    // this auto-enables annotations based on simplestyle data for this map (see http://mapbox.com/developers/simplestyle/ for more info)
-    //
+    self.annotations = [[NSMutableDictionary alloc] init];
+    self.markers = [[NSMutableDictionary alloc] init];
     self.mapView.tileSource = [[RMMapBoxSource alloc] initWithMapID:([[UIScreen mainScreen] scale] > 1.0 ? kRetinaMapID : kNormalMapID)
                                               enablingDataOnMapView:self.mapView];
     
     self.mapView.zoom = 2;
     
-    [self.mapView setConstraintsSouthWest:[self.mapView.tileSource latitudeLongitudeBoundingBox].southWest 
+    [self.mapView setConstraintsSouthWest:[self.mapView.tileSource latitudeLongitudeBoundingBox].southWest
                                 northEast:[self.mapView.tileSource latitudeLongitudeBoundingBox].northEast];
     
     self.mapView.showsUserLocation = YES;
     
     self.title = [self.mapView.tileSource shortName];
-
+    
     // zoom in to markers after launch
     //
+    UITapGestureRecognizer *gestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(hideKeyboard)];
+    //[self.mapView addGestureRecognizer:gestureRecognizer];
     __weak RMMapView *weakMap = self.mapView; // avoid block-based memory leak
-
+    
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, NSEC_PER_SEC), dispatch_get_main_queue(), ^(void)
-    {
-        float degreeRadius = 9000.f / 110000.f; // (9000m / 110km per degree latitude)
-        
-        CLLocationCoordinate2D centerCoordinate = [((RMMapBoxSource *)self.mapView.tileSource) centerCoordinate];
-        
-        RMSphericalTrapezium zoomBounds = {
-            .southWest = {
-                .latitude  = centerCoordinate.latitude  - degreeRadius,
-                .longitude = centerCoordinate.longitude - degreeRadius
-            },
-            .northEast = {
-                .latitude  = centerCoordinate.latitude  + degreeRadius,
-                .longitude = centerCoordinate.longitude + degreeRadius
-            }
-        };
-        
-        [weakMap zoomWithLatitudeLongitudeBoundsSouthWest:zoomBounds.southWest
-                                                northEast:zoomBounds.northEast 
-                                                 animated:YES];
-    });
+                   {
+                       float degreeRadius = 0.5; // (9000m / 110km per degree latitude)
+                       
+                       CLLocationCoordinate2D centerCoordinate = [((RMMapBoxSource *)self.mapView.tileSource) centerCoordinate];
+                       
+                       RMSphericalTrapezium zoomBounds = {
+                           .southWest = {
+                               .latitude  = centerCoordinate.latitude  - degreeRadius,
+                               .longitude = centerCoordinate.longitude - degreeRadius
+                           },
+                           .northEast = {
+                               .latitude  = centerCoordinate.latitude  + degreeRadius,
+                               .longitude = centerCoordinate.longitude + degreeRadius
+                           }
+                       };
+                       
+                       [weakMap zoomWithLatitudeLongitudeBoundsSouthWest:zoomBounds.southWest
+                                                               northEast:zoomBounds.northEast
+                                                                animated:YES];
+                   });
 }
 
-#pragma mark -
-
+//#pragma mark -
+//
 - (void)presentSearch:(id)sender
 {
     NSMutableArray *filterTypes = [NSMutableArray array];
-    
+
     for (RMAnnotation *annotation in self.mapView.annotations)
     {
         if (annotation.userInfo && [annotation.userInfo objectForKey:@"marker-symbol"] && ! [[filterTypes valueForKeyPath:@"marker-symbol"] containsObject:[annotation.userInfo objectForKey:@"marker-symbol"]])
         {
             BOOL selected = ( ! self.activeFilterTypes || [self.activeFilterTypes containsObject:[annotation.userInfo objectForKey:@"marker-symbol"]]);
-            
+
             [filterTypes addObject:[NSMutableDictionary dictionaryWithObjectsAndKeys:
                                        [annotation.userInfo objectForKey:@"marker-symbol"], @"marker-symbol",
                                        [UIImage imageWithCGImage:(CGImageRef)[self mapView:self.mapView layerForAnnotation:annotation].contents], @"image",
@@ -106,60 +104,111 @@
                                        nil]];
         }
     }
-    
+
     MBWPSearchViewController *searchController = [[MBWPSearchViewController alloc] initWithNibName:nil bundle:nil];
-    
+
     searchController.delegate = self;
     searchController.filterTypes = [NSArray arrayWithArray:filterTypes];
-    
+
     UINavigationController *wrapper = [[UINavigationController alloc] initWithRootViewController:searchController];
-    
+
     wrapper.navigationBar.tintColor = self.navigationController.navigationBar.tintColor;
     wrapper.topViewController.title = @"Search";
-    
+
     [self presentModalViewController:wrapper animated:YES];
 }
-
-#pragma mark -
-
+//
+//#pragma mark -
+//
 - (RMMapLayer *)mapView:(RMMapView *)mapView layerForAnnotation:(RMAnnotation *)annotation
 {
     if (annotation.isUserLocationAnnotation)
         return nil;
-
-    RMMarker *marker = [[RMMarker alloc] initWithMapBoxMarkerImage:[annotation.userInfo objectForKey:@"marker-symbol"]
-                                                      tintColorHex:[annotation.userInfo objectForKey:@"marker-color"]
-                                                        sizeString:[annotation.userInfo objectForKey:@"marker-size"]];
-
-    marker.canShowCallout = YES;
-
-    marker.rightCalloutAccessoryView = [UIButton buttonWithType:UIButtonTypeDetailDisclosure];
-
-    if (self.activeFilterTypes)
-        marker.hidden = ! [self.activeFilterTypes containsObject:[annotation.userInfo objectForKey:@"marker-symbol"]];
+    RMMarker *marker;
+    if (annotation.userInfo != NULL)
+    {
+        marker = [[RMMarker alloc] initWithMapBoxMarkerImage:[annotation.userInfo objectForKey:@"marker-symbol"]
+                                                tintColorHex:[annotation.userInfo objectForKey:@"marker-color"]
+                                                  sizeString:[annotation.userInfo objectForKey:@"marker-size"]];
+    }
+    else
+    {
+        marker = [[RMMarker alloc] initWithMapBoxMarkerImage:@"bus" tintColorHex:@"bf3f3f" sizeString:@"small"];
+    }
     
+    marker.canShowCallout = YES;
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    marker.rightCalloutAccessoryView = button;
+    button.frame = CGRectMake(10.0, 140.0, 100.0, 10.0);
+    if (annotation.userInfo != NULL)
+    {
+        [button setTitle:@"Report Late" forState:UIControlStateNormal];
+    } else {
+        [button setTitle:@"On Time" forState:UIControlStateNormal];
+    }
+    //if (self.activeFilterTypes)
+    //  marker.hidden = ! [self.activeFilterTypes containsObject:[annotation.userInfo objectForKey:@"marker-symbol"]];
+    NSLog(@"BACK");
+    if (annotation.userInfo != NULL && [self.annotations valueForKey:[annotation.userInfo  objectForKey:@"title"]]== NULL) {
+        [self.annotations setValue:annotation forKey: [annotation.userInfo objectForKey:@"title"]];
+    }
+    [self.markers setValue:annotation forKey: annotation.title];
     return marker;
 }
-
+//
 - (void)tapOnCalloutAccessoryControl:(UIControl *)control forAnnotation:(RMAnnotation *)annotation onMap:(RMMapView *)map
 {
-    MBWPDetailViewController *detailController = [[MBWPDetailViewController alloc] initWithNibName:nil bundle:nil];
-
-    detailController.detailTitle       = [annotation.userInfo objectForKey:@"title"];
-    detailController.detailDescription = [annotation.userInfo objectForKey:@"description"];
-
-    [self.navigationController pushViewController:detailController animated:YES];
+    [self.view endEditing:YES];
+    NSLog(@"%@", annotation.userInfo);
+    [self.mapView removeAnnotation: annotation];
+    RMMarker *marker = [[RMMarker alloc] initWithMapBoxMarkerImage:[annotation.userInfo objectForKey:@"marker-symbol"]
+                                                      tintColorHex:[annotation.userInfo objectForKey:@"#bf3f3f"]
+                                                        sizeString:[annotation.userInfo objectForKey:@"marker-size"]];
+    //NSLog(@"Delete");
+    if (annotation.userInfo == NULL)
+    {
+        annotation = [self.annotations valueForKey:annotation.title];
+    }
+    else
+    {
+        annotation = [RMAnnotation annotationWithMapView: self.mapView coordinate: annotation.coordinate andTitle: annotation.title];
+    }
+    //NSLog(@"%@", annotation.userInfo);
+    [self.mapView addAnnotation: annotation];
 }
-
-#pragma mark -
-
-- (void)searchViewController:(MBWPSearchViewController *)controller didApplyFilterTypes:(NSArray *)filterTypes
+- (void)hideKeyboard
 {
-    self.activeFilterTypes = filterTypes;
-    
-    for (RMAnnotation *annotation in self.mapView.annotations)
-        if ( ! annotation.isUserLocationAnnotation)
-            annotation.layer.hidden = ! [self.activeFilterTypes containsObject:[annotation.userInfo objectForKey:@"marker-symbol"]];
+
+    [self.view endEditing:YES];
 }
+- (void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText
+{
+    if ([self.markers valueForKey: searchText] != NULL) {
+        NSArray *places =  self.markers.allValues;
+        for (int i = 0; i < places.count; i++) {
+            RMAnnotation *place = [places objectAtIndex:i];
+            if (![place.title isEqualToString:searchText]) {
+                NSLog(@"%@", place.title);
+                [self.mapView removeAnnotation: place];
+            }
+        }        NSLog(@"here");
+    }
+    if ([searchText isEqualToString:@""]) {
+        [self.mapView removeAllAnnotations];
+        NSArray *places =  self.markers.allValues;
+        for (int i = 0; i < places.count; i++) {
+            RMAnnotation *place = [places objectAtIndex:i];
+            [self.mapView addAnnotation: place];
+
+        }
+        
+    }
+    
+}
+-(void)searchBarCancelButtonClicked:(UISearchBar *)searchBar
+{
+    [self.view endEditing:YES];
+}
+
 
 @end
